d2c12671ab06fc084e0af521a0309acb77bcdd35
Ian Feather <info@ianfeather.co.uk>
2020-06-12 22:45:55 +0100

    Introduce api proxt

--- go-src/recipes/main.go
+++ go-src/recipes/main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
+	"github.com/gorilla/mux"
 )
 
-func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	fmt.Println(request)
-	return &events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Hello, World",
-	}, nil
+var muxLambda *gorillamux.GorillaMuxAdapter
+
+func fooHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello FOO")
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hello BAR")
+}
+
+func init() {
+	fmt.Printf("GMux Cold Start")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", fooHandler)
+	r.HandleFunc("/bar", barHandler)
+
+	muxLambda = gorillamux.New(r)
+}
+
+func handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(req)
+	return muxLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
-	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.14
 
 require (
 	github.com/aws/aws-lambda-go v1.13.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.6.0
+	github.com/gorilla/mux v0.0.0-20180120075819-c0091a029979
 )

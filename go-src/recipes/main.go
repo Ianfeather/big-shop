9ddbe323d5704bc17a6e9e011563235f977ad174
Ian Feather <info@ianfeather.co.uk>
2020-06-12 22:23:55 +0100

    log request

--- go-src/recipes/main.go
+++ go-src/recipes/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+	fmt.Println(request)
 	return &events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello, World",

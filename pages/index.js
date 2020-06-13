b7ed129ea641fc244296809a06a7413a1ce7cda4
Ianfeather <info@ianfeather.co.uk>
2020-06-14 00:13:26 +0100

    Ui (#3)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,83 @@
+import styles from './index.module.css';
+import useFetch from 'use-http'
+import { useState, useEffect } from 'react';
 import Layout from '@components/Layout'
 
 const Index = ({ title, description, ...props }) => {
+  let [recipes, setRecipes] = useState([]);
+  let [recipeList, setRecipeList] = useState({});
+  let [shoppingList, setShoppingList] = useState({});
+
+  const handleRecipeSelect = (e) => {
+    const newList = { ...recipeList,
+      [e.target.id]: !recipeList[e.target.id]
+    };
+    setRecipeList(newList);
+  };
+
+  const { get, response, loading, error } = useFetch('/.netlify/functions/big-shop')
+
+  async function getRecipes() {
+    const recipes = await get('/recipes')
+    if (response.ok) setRecipes(recipes)
+  };
+
+  async function getShoppingList() {
+    const selectedRecipes = Object.keys(recipeList).filter(k => !!recipeList[k]);
+    if (selectedRecipes.length) {
+      const recipeIds = selectedRecipes.join(',');
+      const { list } = await get(`/shopping-list?recipes=${recipeIds}`);
+      if (response.ok) setShoppingList(list);
+      if (response.error || error) setShoppingList({});
+    } else {
+      setShoppingList({});
+    }
+  };
+
+  useEffect(() => { getRecipes() }, []);
+  useEffect(() => { getShoppingList() }, [recipeList]);
+
   return (
     <Layout pageTitle={title} description={description}>
-      <h1 className="title">{title}</h1>
+      <section>
+        <div className={styles.grid}>
+          <div className={styles.recipeList}>
+            <h2>Recipes</h2>
+            <ul>
+              {
+                recipes.map(({id, name}) => {
+                  let checked = recipeList[id];
+                  return (
+                    <li key={id} className={checked ? styles.checked : ''}>
+                      <label htmlFor={id}>
+                        {name}
+                        <input type="checkbox" id={id} className={styles.hidden} onChange={handleRecipeSelect}/>
+                      </label>
+                    </li>
+                  );
+                })
+              }
+            </ul>
+          </div>
+          <div>
+            <h2>Your shopping list</h2>
+            <ul className={styles.shoppingList}>
+              {
+                Object.keys(shoppingList).map(name => {
+                  const { unit, quantity } = shoppingList[name];
+                  return (
+                    <li className={styles.item} key={name}>
+                      <span className={styles.itemName}>{name}</span>
+                      <span className={styles.itemQuantity}>{quantity}</span>
+                      <span className={styles.itemUnit}>{unit}</span>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }

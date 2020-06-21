cbe8e10cad1f2542096e9a3f2aca74f09e7c68d5
Ian Feather <info@ianfeather.co.uk>
2020-06-21 23:22:03 +0100

    Typeahead

--- components/recipe-form/Form.js
+++ components/recipe-form/Form.js
@@ -1,29 +1,40 @@
 import styles from './form.module.css';
 import { useState, useEffect } from 'react';
 import useFetch from 'use-http'
+import { Typeahead } from 'react-typeahead';
 
-export default function Form({initialRecipe = {}}) {
+
+export default function Form({initialRecipe}) {
   const bareIngredient = { name: '', quantity: '', unit: '' };
-  let [recipe, setRecipe] = useState(initialRecipe);
+  const bareRecipe = { ingredients: [bareIngredient]};
+
+  let [recipe, setRecipe] = useState(initialRecipe || bareRecipe);
   let [saved, setSaved] = useState(false);
-  let [ingredients, setIngredients] = useState([bareIngredient]);
-  let [units, setUnits] = useState([{name:'g', id:1}, {name:'kg', id:2}]);
+  let [units, setUnits] = useState([]);
+  let [ingredients, setIngredients] = useState([]);
 
   const { get, post, put, response, loading, error } = useFetch('/.netlify/functions/big-shop')
 
   useEffect(() => {
-    if (initialRecipe.name) {
-      setRecipe(initialRecipe);
-      setIngredients([ ...initialRecipe.ingredients, bareIngredient]);
+    if (initialRecipe && initialRecipe.name) {
+      setRecipe({
+        ...initialRecipe,
+        ingredients: [ ...initialRecipe.ingredients, bareIngredient ]
+      });
     }
   }, [initialRecipe]);
 
-  async function getUnits() {
-    const units = await get('/units')
-    if (response.ok) setUnits(units)
+  async function getUnitsAndIngredients() {
+    const [_units, _ingredients] = await Promise.all([
+      get('/units'),
+      get('/ingredients')
+    ]);
+    if (response.ok) {
+      setUnits(_units)
+      setIngredients(_ingredients.map(i => i.name))
+    }
   };
-
-  useEffect(() => { getUnits() }, []);
+  useEffect(() => { getUnitsAndIngredients() }, []);
 
   function updateRecipe(key, value) {
     const updatedRecipe = { ...recipe, [key]: value};
@@ -31,17 +42,20 @@ export default function Form({initialRecipe = {}}) {
   }
 
   function updateIngredient(i, key, value) {
-    let newIngredients = [...ingredients];
+    let newIngredients = [...recipe.ingredients];
     newIngredients[i][key] = value;
-    if (i === ingredients.length - 1) {
+    if (i === recipe.ingredients.length - 1) {
       newIngredients.push(bareIngredient);
     }
-    setIngredients(newIngredients);
+    setRecipe({
+      ...recipe,
+      ingredients: newIngredients
+    });
   }
 
   async function submitRecipe(e) {
     e.preventDefault();
-    const completeRecipe = { ...recipe, ingredients: ingredients.filter(({name}) => !!name)};
+    const completeRecipe = { ...recipe, ingredients: recipe.ingredients.filter(({name}) => !!name)};
     let result;
     if (recipe.id) {
       result = await put('/recipe', completeRecipe)
@@ -55,9 +69,12 @@ export default function Form({initialRecipe = {}}) {
 
   function deleteIngredient(e) {
     e.preventDefault();
-    const newIngredients = ingredients.filter((_, idx) => {
-      return idx !== Number(e.target.id)
-    });
+    setRecipe({
+      ...recipe,
+      ingredients: recipe.ingredients.filter((_, idx) => {
+        return idx !== Number(e.target.id)
+      })
+    })
   }
 
   return (
@@ -74,11 +91,17 @@ export default function Form({initialRecipe = {}}) {
 
       <h2>Ingredients</h2>
       {
-        ingredients.map((ingredient, i) => (
+        recipe.ingredients.map((ingredient, i) => (
           <div className={styles.ingredientGroup} key={i}>
             <div className={styles.ingredientName}>
               <label htmlFor="ingredient-name">Ingredient Name</label>
-              <input value={ingredient.name} autoComplete="off" type="text" id="ingredient-name" onChange={(e) => updateIngredient(i, 'name', e.target.value)}/>
+              <Typeahead
+                id="ingredient-name"
+                options={ingredients}
+                maxVisible={3}
+                value={ingredient.name}
+                onChange={(e) => updateIngredient(i, 'name', e.target.value)}
+                onOptionSelected={(value) => updateIngredient(i, 'name', value)} />
             </div>
             <div>
               <label htmlFor="ingredient-quantity">Quantity</label>
@@ -106,8 +129,14 @@ export default function Form({initialRecipe = {}}) {
           </div>
         ))
       }
-      <button className={`${styles.button} ${loading ? styles.loading : ''}`} onClick={submitRecipe}>Store Recipe</button>
-      { saved && <div className={styles.stored}>Stored!</div> }
+      <button className={`${styles.button} ${loading ? styles.loading : ''}`} onClick={submitRecipe}>
+        { recipe.id ? 'Update Recipe' : 'Store Recipe'}
+      </button>
+      { saved && (
+        <div className={styles.stored}>
+          { recipe.id ? 'Updated!' : 'Stored!'}
+        </div>
+      )}
     </form>
     </>
   )

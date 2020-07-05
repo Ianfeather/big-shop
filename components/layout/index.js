0fea8d0abe2712a2ae546cf30db35052e15bed8e
Ian Feather <info@ianfeather.co.uk>
2020-07-05 18:50:35 +0100

    apple icons

--- components/layout/index.js
+++ components/layout/index.js
@@ -8,9 +8,14 @@ export default function Layout({ children, pageTitle, description, ...props }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="Description" content={description}></meta>
+        <meta name="mobile-web-app-capable" content="yes"/>
+        <meta name="theme-color" content="#b870eb" />
+        <meta name="apple-mobile-web-app-capable" content="yes"/>
         <title>{pageTitle}</title>
         <link rel="manifest" href="/manifest.json" />
         <link rel="shortcut icon" crossOrigin="" href="/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" href="/static/icon512.png"/>
+        <link href="/static/icon512.png" rel="apple-touch-startup-image" />
       </Head>
       <section className="layout">
         <Header title={pageTitle}/>

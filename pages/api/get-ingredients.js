bfadd779a89ffa92e93209a4c510cbc1c3251d20
Ian Feather <info@ianfeather.co.uk>
2021-12-28 00:18:38 +0000

    working bbc food scraper api

--- pages/api/get-ingredients.js
+++ pages/api/get-ingredients.js
@@ -1,47 +1,56 @@
-const jsDom = require('js-dom');
+import { parse } from 'node-html-parser';
+
+const defaultScraper = {
+  getList(document) {
+    return []
+  },
+  regex: /(?<quantity>\d+)(?: )?(?:(?<unit>[a-zA-Z]{1,4})?) (?<ingredient>[\w| ]+)/
+}
 
 const BBCGoodFood = {
-  getElement() {
+  getList(document) {
     const headings = document.querySelectorAll('h1,h2,h3,h4');
+    const ingredientHeading = [...headings].filter(heading => (
+      heading.innerText.match(/ingredients?/ig)
+    ));
+    const list = ingredientHeading[0].parentNode.querySelector('ul, ol').querySelectorAll('li');
+    return Array.from(list).map(li => li.innerText);
   },
 
-  regex: /(?<quantity>\d+)(?: )?(?:(?<unit>[a-zA-Z]{1,4})?) (?<ingredient>.+)/
+  regex: /(?<quantity>\d+)(?: )?(?:(?<unit>[a-zA-Z]{1,4})?) (?<ingredient>[\w| ]+)/
 }
 
 const hostnameMap = {
   'www.bbcgoodfood.com': BBCGoodFood
 }
 
-function getList(document, hostname) {
-  return hostnameMap[hostname].getElement(document)
+function getList(html, hostname) {
+  const document = parse(html);
+  return (hostnameMap[hostname] || defaultScraper).getList(document)
+}
+
+function parseList(list, hostname) {
+  return list.map(li => {
+    const result = li.match((hostnameMap[hostname] || defaultScraper).regex);
+    const { quantity, unit, ingredient } = result?.groups || {};
+    // TODO: handle missing ones? Tidy up the response
+    return {
+      text: li, quantity, unit, ingredient
+    }
+  })
 }
 
 export default async function handler(req, res) {
-
-  const url = req.params.url;
-  const { hostname } = new URL(url);
-  const content = fetch(url);
-  const document = jsDom.render(content);
-  const list = getList(document, hostname);
-
-
-  const ingredientHeading = [...headings].filter(heading => {
-    return heading.innerText.match(/ingredients?/ig)
-  });
-
-  if (ingredientHeading.length === 1) {
-      const list = ingredientHeading[0].parentNode.querySelector('ul, ol').querySelectorAll('li');
-      const ingredients = [...list].map(li => {
-        const text = li.innerText.split(',')[0];
-        const regex = /(?<quantity>\d+)(?: )?(?:(?<unit>[a-zA-Z]{1,4})?) (?<ingredient>.+)/;
-        const result = text.match(regex);
-        const { quantity, unit, ingredient } = result?.groups || {};
-        return {
-          text, quantity, unit, ingredient
-        }
-      })
-      return res.status(200).json(ingredients);
-  } else {
-    // ??
+  const url = req.query.url;
+  try {
+    const { hostname } = new URL(url);
+    const html = await(await fetch(url)).text();
+    const list = getList(html, hostname);
+    const ingredients = parseList(list, hostname);
+    res.status(200).json(ingredients);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'failed to load data' });
   }
+
 }

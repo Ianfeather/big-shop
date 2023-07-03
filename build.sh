46f61f6c573214afbf14fd54dae159d73dc89a60
Ian Feather <info@ianfeather.co.uk>
2023-07-03 14:45:13 +0100

    change build dir for fns

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 npm run package
 
-rm -rf netlify/functions/recipes
-mkdir -p netlify/functions/recipes
+rm -rf netlify-functions/recipes
+mkdir -p netlify-functions/recipes
 cd recipe-app
 go fmt ./...
 go test ./... -v
-GOOS=linux GOARCH=amd64 go build -o ../netlify/functions/recipes/main cmd/main.go
+GOOS=linux GOARCH=amd64 go build -o ../netlify-functions/recipes/main cmd/main.go

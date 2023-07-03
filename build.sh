46540908890cc834ee7dc7f963a1f38cc5a8003e
Ian Feather <info@ianfeather.co.uk>
2023-07-03 15:01:20 +0100

    bump

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@
 
 npm run package
 
-rm -rf netlify-functions/recipes
-mkdir -p netlify-functions/recipes
-cd recipe-app
-go fmt ./...
-go test ./... -v
-GOOS=linux GOARCH=amd64 go build -o ../netlify-functions/recipes/main cmd/main.go
-ls -la ../netlify-functions/recipes
+# rm -rf netlify-functions/recipes
+# mkdir -p netlify-functions/recipes
+# cd recipe-app
+# go fmt ./...
+# go test ./... -v
+# GOOS=linux GOARCH=amd64 go build -o ../netlify-functions/recipes/main cmd/main.go
+# ls -la ../netlify-functions/recipes

46816ab149c01fb2407cc7adfe7ee64187a95a7a
Ian Feather <info@ianfeather.co.uk>
2024-04-16 00:34:08 +0100

    all

--- components/sidebar-tag-filter/index.js
+++ components/sidebar-tag-filter/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.css';
 const SidebarTagFilter = ({ onChange, tags, value }) => {
   return (
     <div className={styles.container}>
-      <button className={`${styles.btn} ${value=='' ? styles.selected : ''}`} onClick={() => onChange([])}>All</button>
+      <button className={`${styles.btn} ${value=='' ? styles.selected : ''}`} onClick={() => onChange('')}>All</button>
       {
         tags.map(tag => (
           <button key={tag.name} className={`${styles.btn} ${value==tag.name ? styles.selected : ''}`} onClick={(e) => onChange(e.target.innerText)}>{tag.name}</button>
